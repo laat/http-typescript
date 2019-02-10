f0608f8b7373577580f2105920434b2964e59ee2
Sigurd Fosseng <sigurd@fosseng.net>
2019-02-10 22:27:42 +0100

    add more examples

---
+++ examples/http-bin/delay/{delay}/GET.v1.0.test.ts
@@ -0,0 +1,8 @@
+describe("GET v1.0 /delay/{delay}", () => {
+  it("should match example json to TypeScript definition", () => {
+    expect(require("./__fixtures__/GET.v1.0.test.json")).toMatchType(
+      "Response",
+      require.resolve("./GET.v1.0.ts")
+    );
+  });
+});
---
+++ examples/http-bin/readme.md
@@ -0,0 +1,13 @@
+# https://httpbin.org/ typings example
+
+Generate typings for a new endpoint with
+
+`yarn new`
+
+Test typings against fixtures
+
+`yarn test`
+
+Test typings against production
+
+`yarn test-production`
